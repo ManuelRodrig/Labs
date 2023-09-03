2924790ec9cbcf5db0a982cfd501d85a59b2495b
Manuel Rodriguez <manrp1761998@gmail.com>
2023-09-02 22:40:28 -0500

    cambio lab14

--- lab1/lab14.php
+++ lab1/lab14.php
@@ -5,9 +5,9 @@
             <?php
               echo "<table border=1>";
               $n=1;
-              for ($n1;$n1<10;$n1++){
+              for ($n1=1;$n1<10;$n1++){
                 echo "tr";
-                for($n2;$n2<10;$n2++){
+                for($n2=1;$n2<10;$n2++){
                     echo "<td>", $n, "</td>";
                     $n=$n+1; 
                 }
