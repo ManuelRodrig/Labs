b74cf2851cc9c79d2e8f9075db71584d799486e1
Manuel Rodriguez <manrp1761998@gmail.com>
2023-09-02 21:20:40 -0500

    añadir lab23.php

---
+++ lab2/lab23.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>laboratorio 2.3</title>
+    </head>
+    <body>
+        <center>
+            print("<UL>\n");
+            $i=1;
+            while($i<= 5)
+            {
+                print ("<LI>Elemento $i"</LI>);
+                $i=++;
+            }
+            print("<UL>\n");
+        </center>
+    </body>
+</html>
