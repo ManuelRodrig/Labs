d57c5888fd21c8fbd53e729a356297dc56422081
Manuel Rodriguez <manrp1761998@gmail.com>
2023-09-02 21:23:22 -0500

    cambios lab23

--- lab2/lab23.php
+++ lab2/lab23.php
@@ -4,14 +4,16 @@
     </head>
     <body>
         <center>
+            <?php
             print("<UL>\n");
             $i=1;
             while($i<= 5)
             {
-                print ("<LI>Elemento $i"</LI>);
-                $i=++;
+                print ("<LI>Elemento $i</LI>\n");
+                $i++;
             }
             print("<UL>\n");
+            ?>
         </center>
     </body>
 </html>
