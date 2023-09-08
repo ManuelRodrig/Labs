07e4f419f6e47967385592857a29e374731df18d
Manuel Rodriguez <manrp1761998@gmail.com>
2023-09-07 19:25:56 -0500

    cambios lab 32

--- lab3/Lab32/lab32.php
+++ lab3/Lab32/lab32.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html>
     <?php
-    $precio1 = $_POS['precio1'];
-    $precio2 = $_POS['precio2'];
-    $precio3 = $_POS['precio3'];
+    $precio1 = $_POST['precio1'];
+    $precio2 = $_POST['precio2'];
+    $precio3 = $_POST['precio3'];
     $media = ($precio1+$precio2+$precio3)/3;
-    echo "<br/> DATOS RECIBIDOS";
+    echo "<br/> DATOS RECIBIDOS <br/>";
     echo "<br/> Precio establecimiento 1: ".$precio1." dolares";
     echo "<br/> Precio establecimiento 1: ".$precio2." dolares";
     echo "<br/> Precio establecimiento 1: ".$precio3." dolares <br/>";
