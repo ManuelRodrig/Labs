4652c76bb2f83667378fa563ffe3a7159bb1950e
Manuel Rodriguez <manrp1761998@gmail.com>
2023-09-17 01:45:16 -0500

    Lab 51 y 52

--- lab4/lab45/lab45.php
+++ lab4/lab45/lab45.php
@@ -5,8 +5,8 @@
             <?php
               echo "<table border=1>";
               $n = 1;
-              for ($n1=1;$n1<=4;$n1++){
-                for($n2=1;$n2<=4;$n2++){
+              for ($n1=1;$n1<=30;$n1++){
+                for($n2=1;$n2<=30;$n2++){
                     $n = ((pow($n1,1))-$n2)+1;
                     if($n==1){
                         echo "<td>", $n, "</td>";
